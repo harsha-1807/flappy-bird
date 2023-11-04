3bd2d8e1e50f9c5d3c702b1e14cea696cf0063b8
harsha-1807 <harshavardhan.sr@kalvium.community>
2023-11-04 23:57:57 +0530

    html css

---
+++ Pages/instructions page/ins.js
@@ -0,0 +1,11 @@
+let button1 = document.getElementById("button1")
+
+
+
+  
+button1.onclick = () => {
+      window.location.href="./Pages/start page/start.html","_self"
+};
+
+
+// /Pages/start page/start.html
---
+++ Pages/start page/start.js
@@ -0,0 +1,17 @@
+let button1 = document.getElementById("button1")
+let button2 = document.getElementById("button2")
+let button3 = document.getElementById("button3")
+
+
+button1.onclick = () => {
+  window.open("./Pages/name page/name.html","_self")
+  };
+
+  button2.onclick = () => {
+    window.open("./Pages/name page/name.html","_self")
+  };
+
+  button3.onclick = () => {
+    window.open("/Pages/instructions page/ins.html","_self")
+  };
+
